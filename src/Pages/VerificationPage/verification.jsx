5c3e160737422fc33e6b6ad46e02594675a3370b
Eggi19 <eggiyapari19@gmail.com>
2023-06-16 00:47:41 +0800

    master

--- src/Pages/VerificationPage/verification.jsx
+++ src/Pages/VerificationPage/verification.jsx
@@ -1,25 +1,62 @@
 import { useEffect, useState } from "react"
-import { useSearchParams } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 import { userVerification } from "../../API/userAPI"
+import Button from '@mui/material/Button';
 
-export default function VerificationPage(){
+export default function VerificationPage() {
     const [searchParams, setSearchParams] = useSearchParams()
+    const navigate = useNavigate()
 
     const verifiedAccount = async () => {
         try {
             let token = searchParams.get('token')
             await userVerification(token)
         } catch (error) {
-            
+
         }
     }
 
     useEffect(() => {
         verifiedAccount()
     }, [])
-    return(
+    return (
         <>
-            <h1>Account has been verified</h1>
+            <link
+                href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap"
+                rel="stylesheet"
+            />
+            <style
+                dangerouslySetInnerHTML={{
+                    __html:
+                        'body {text-align: center; padding: 40px 0;  background: #EBF0F5;}  h1 {color: #88B04B;    font-family: "Nunito Sans", "Helvetica Neue", sans-serif;    font-weight: 900;    font-size: 40px;    margin-bottom: 10px;  }  p {    color: #404F5E;    font-family: "Nunito Sans", "Helvetica Neue", sans-serif;    font-size:20px;    margin: 0;  }i {  color: #9ABC66;  font-size: 100px;  line-height: 200px;  margin-left:-15px;}.card {  background: white;  padding: 60px;  border-radius: 4px;  box-shadow: 0 2px 3px #C8D0D8;  display: inline-block;  margin: 0 auto;}'
+                }}
+            />
+            <div className="card">
+                <div
+                    style={{
+                        borderRadius: 200,
+                        height: 200,
+                        width: 200,
+                        background: "#F8FAF5",
+                        margin: "0 auto"
+                    }}
+                >
+                    <i className="checkmark">✓</i>
+                </div>
+                <h1>Success</h1>
+                <p>
+                    Your account has been verified
+                    <br />
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        onClick={() => navigate('/login')}
+                    >
+                        Login
+                    </Button>
+                </p>
+            </div>
         </>
     )
 }
